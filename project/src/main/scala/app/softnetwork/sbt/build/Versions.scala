3fe2af286bdfcc9e7ac61878fa8cefa94ad927ba
Stéphane Manciot <stephane.manciot@gmail.com>
2023-01-25 09:39:13 +0100

    upgrade to generic persistence api v0.2.6.1

--- project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
+++ project/src/main/scala/app/softnetwork/sbt/build/Versions.scala
@@ -2,9 +2,9 @@ package app.softnetwork.sbt.build
 
 object Versions {
 
-  val genericPersistence = "0.2.6"
+  val genericPersistence = "0.2.6.1"
 
-  val server = "0.2.7"
+  val server = "0.2.8"
 
   val scalatest = "3.1.1"
 }
